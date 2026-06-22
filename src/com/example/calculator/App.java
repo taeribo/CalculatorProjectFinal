087402bd90054a2daca2e804af1f63b8db80651c
taeheon <psw4497@gamil.com>
2026-06-22 22:14:05 +0900

    0을 포함한 양의 정수 2개와 연산 기호 입력받아 결과 출력

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -5,42 +5,48 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        if(num1 <0){
-            System.out.println("0을 포함한 양의 정수를 입력하세요.");
-        }
+        String exit = "";
+        while (!exit.equals("exit"))
+        {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            if (num1 < 0) {
+                System.out.println("0을 포함한 양의 정수를 입력하세요.");
+            }
 
 
-        System.out.println("두 번째 숫자를 입력하세요.");
-        int num2 = sc.nextInt();
-        if(num2 <0){
-            System.out.println("0을 포함한 양의 정수를 입력하세요 ");
-        }
+            System.out.println("두 번째 숫자를 입력하세요.");
+            int num2 = sc.nextInt();
+            if (num2 < 0) {
+                System.out.println("0을 포함한 양의 정수를 입력하세요 ");
+            }
 
-        System.out.println("사칙 연산 기호(+,-,*,/)를 입력하세요");
-        char op = sc.next().charAt(0);
-        System.out.println("연산 기호 : " + op);
+            System.out.println("사칙 연산 기호(+,-,*,/)를 입력하세요");
+            char op = sc.next().charAt(0);
+            System.out.println("연산 기호 : " + op);
 
-        int result;
-        switch (op) {
-            case '+' -> result = num1 + num2;
-            case '-' -> result = num1 - num2;
-            case '*' -> result = num1 * num2;
-            case '/' -> {
-                if (num2 == 0) {
-                    System.out.println("0으로 나눌 수 없습니다.");
-                    return;
+            int result;
+            switch (op) {
+                case '+' -> result = num1 + num2;
+                case '-' -> result = num1 - num2;
+                case '*' -> result = num1 * num2;
+                case '/' -> {
+                    if (num2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        return;
+                    }
+                    result = num1 / num2;
                 }
-                result = num1 / num2;
-            }
-            default -> {
-                System.out.println("지원하지 않는 연산자");
-                return;
+                default -> {
+                    System.out.println("지원하지 않는 연산자");
+                    return;
 
+                }
             }
+            System.out.println("결과 : " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력시 종료) / 계속 하려면 아무 키 입력.");
+            exit = sc.next();
         }
-        System.out.println("결과 : " + result);
     }
 }
 
